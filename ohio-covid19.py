caf937891e8e2201883658e2cd7af943b4a2bbdf
Chris Martin <chrismartin@riseup.net>
2020-11-12 09:27:18 -0500

    add 7 day stats to graph / update styling

--- ohio-covid19.py
+++ ohio-covid19.py
@@ -57,7 +57,15 @@ def get_last_update_timestamp(data):
     for county in data:
         for date in all_data[county].keys():
             all_dates.append(date)
-    return str(max(all_dates))
+    return str(max(all_dates).date())
+
+def get_num_cases_over_time_peroid(data, list_of_dates):
+    num_cases = 0
+    for county in data:
+        for date in all_data[county].keys():
+            if date.date() in list_of_dates:
+                num_cases += all_data[county][date]['Case Count']
+    return num_cases
 
 def file_prepend(input_file, string):
     with open(input_file, 'r+') as f:
@@ -69,6 +77,17 @@ download_data()
 all_data = load_data(DATA_FILE)
 top_data = get_top_counties(20, all_data)
 last_update_timestamp = get_last_update_timestamp(all_data)
+
+today = datetime.datetime.now().date()
+seven_day_offset = datetime.timedelta(days = 7)
+seven_days_ago = today - seven_day_offset
+fourteen_days_ago = seven_days_ago - seven_day_offset
+past_seven_days = [seven_days_ago + datetime.timedelta(days=x) for x in range((today-seven_days_ago).days + 1)]
+previous_seven_days = [fourteen_days_ago + datetime.timedelta(days=x) for x in range((seven_days_ago-fourteen_days_ago).days + 1)]
+
+current_seven_day_cases = get_num_cases_over_time_peroid(all_data, past_seven_days)
+previous_seven_day_cases = get_num_cases_over_time_peroid(all_data, previous_seven_days)
+seven_day_change = str(round((current_seven_day_cases - previous_seven_day_cases) / previous_seven_day_cases * 100, 2)) + "%"
 
 time_ranges = [200, 30, 7]
 fig, axs = plt.subplots(nrows=len(time_ranges), ncols=1, figsize=(10, 10))
@@ -90,5 +109,19 @@ plt.tight_layout()
 
 mpld3.save_html(fig, 'index.html')
 
-last_update_timestamp_string = "<h3>Last updated: " + last_update_timestamp + "</h3>"
-file_prepend('index.html', last_update_timestamp_string)
+seven_day_change_html = "<h3>Change: " + seven_day_change + "</h3>"
+previous_seven_day_cases_html = "<h3>Previous seven day cases: " + str(previous_seven_day_cases) + "</h3>"
+current_seven_day_cases_html = "<h3>Current seven day cases: " + str(current_seven_day_cases) + "</h3>"
+style_html = "<style>body { text-align: center; font-family: monospace; margin-top: 1%; }</style>"
+last_update_timestamp_html = "<h4>Last updated: " + last_update_timestamp + "</h4>"
+schools_link_html = "<a href='schools/index.html'>school data</a>"
+
+file_prepend('index.html', seven_day_change_html)
+file_prepend('index.html', previous_seven_day_cases_html)
+file_prepend('index.html', current_seven_day_cases_html)
+file_prepend('index.html', style_html)
+
+html_file = open('index.html', "a")
+html_file.writelines(last_update_timestamp_html)
+html_file.writelines(schools_link_html)
+html_file.close()
