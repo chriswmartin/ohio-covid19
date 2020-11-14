b6c6183d39d957ca659aafe5f8d8ca01ddb74184
Chris Martin <chrismartin@riseup.net>
2020-11-14 09:33:39 -0500

    add more school data

--- schools/schools.py
+++ schools/schools.py
@@ -1,7 +1,8 @@
-import csv
+import io
 import re
 import locale
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 from rich.console import Console
 from rich.table import Table
@@ -13,12 +14,9 @@ URL = "https://coronavirus.ohio.gov/static/dashboards/school_reporting.csv"
 FILE = "data/school_reporting.csv"
 
 def download_data(url):
-    data = {}
-    with requests.Session() as s:
-        download = s.get(url)
-        decoded_content = download.content.decode('utf-8')
-        reader = csv.reader(decoded_content.splitlines(), delimiter=',')
-        data = {rows[1]:rows[4] for rows in reader}
+    urlData = requests.get(url).content
+    df = pd.read_csv(io.StringIO(urlData.decode('utf-8')), usecols=['school_or_school_district', 'student_cases_new', 'student_cases_cumulative', 'staff_cases_new', 'staff_cases_cumulative'], header=0)
+    data = df.to_dict(orient='records')
     return data
 
 def get_num_students(school):
@@ -45,33 +43,51 @@ def file_prepend(input_file, string):
         f.seek(0, 0)
         f.write(string.rstrip('\r\n') + '\n' + content)
 
-data = download_data(URL)
+raw_data = download_data(URL)
 
-for key, value in list(data.items()):
-    if value.isnumeric():
-        data[key] = int(value)
-    else:
-        del data[key]
+data = []
+for entry in raw_data:
+    total_cases = 0
+
+    new_cases = ['student_cases_new', 'staff_cases_new']
+    cumulative_cases = ['student_cases_cumulative', 'staff_cases_cumulative']
+    
+    for case in cumulative_cases:
+        if entry[case].isnumeric():
+            entry[case] = int(entry[case])
+            total_cases += entry[case]
+        else:
+            entry[case] = 0
+    entry['total_cases'] = total_cases
+    
+    for case in new_cases:
+        if entry[case].isnumeric() == False:
+            entry[case] = 0
+    data.append(entry)
 
 table = Table(title="Top Schools")
 table.add_column("School name",  style="blue", no_wrap=True)
-table.add_column("Total cases", style="red", no_wrap=True)
-table.add_column("Number of students", style="green", no_wrap=True)
-table.add_column("Percentage", style="cyan", no_wrap=True)
+table.add_column("Total cases", style="red", no_wrap=False)
+table.add_column("New student cases", style="magenta", no_wrap=False)
+table.add_column("New staff cases", style="magenta", no_wrap=False)
+table.add_column("Number of students (est.)", style="green", no_wrap=False)
+table.add_column("Percentage (est.)", style="cyan", no_wrap=False)
 
-NUMBER_OF_SCHOOLS = 40
-top_schools = sorted(data, key=data.get, reverse=True)[:NUMBER_OF_SCHOOLS]
+NUMBER_OF_SCHOOLS = 25
+top_schools = sorted(data, key=lambda i: i['total_cases'], reverse=True)[:NUMBER_OF_SCHOOLS]
 for school in track(top_schools):
-    school_name = school
-    number_of_students = get_num_students(school)
+    school_name = school['school_or_school_district']
+    total_cases = school['total_cases']
+    new_student_cases = school['student_cases_new']
+    new_staff_cases = school['staff_cases_new']
+    number_of_students = get_num_students(school_name)
     if number_of_students is None:
         number_of_students = "N/A"
-    total_cases = data[school]
     if type(number_of_students) is int:
         percentage = "{:.2%}".format(total_cases / number_of_students)
     else:
         percentage = "N/A"
-    table.add_row(str(school_name), str(total_cases), str(number_of_students), str(percentage))
+    table.add_row(str(school_name), str(total_cases), str(new_student_cases), str(new_staff_cases), str(number_of_students), str(percentage))
 
 console = Console(record=True)
 console.print(table)
